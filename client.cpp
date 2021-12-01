06f77708bb25a8d3744045041b3f6625207d71f9
moon657 <62825052+moon657@users.noreply.github.com>
2021-12-01 17:10:40 +0800

    fix empty Packet send and recv

--- client.cpp
+++ client.cpp
@@ -1,4 +1,6 @@
-// name:shep
+// name:shepherd
+
+
 #include <cstdio>
 #include <stdlib.h>
 #include <string>
@@ -13,48 +15,52 @@
 using namespace std;
 
 #define SERVER_PORT 9999
-#define BUFFER_SIZE 40960
-typedef struct{
-int cacheID;
-int taskType;
-char key[1024];
-char value[1024];
-}Packet;
+#define BUFFER_SIZE 4096
+
+#define INSERT 0
+#define GETVALUE 1
+const string VALUETEST = "valuetest";
+typedef struct {
+    int cacheID;
+    int taskType;
+    char key[1024];
+    char value[1024];
+} recvInfo;
 
 int main(int argc, char **argv) {
+    recvInfo revbuf;
     char serverAddress[100];
-    int  serverPort;
-    int    connSock;
+    int serverPort;
+    int connSock;
     bool startDownload = false;
-	Packet packet;
-    struct  sockaddr_in servAddr;
-    char sentBuf[BUFFER_SIZE], recvBuf[BUFFER_SIZE];
-    int  sentDataLen, recvDataLen;
+    struct sockaddr_in servAddr;
+    char sentcharBuf[BUFFER_SIZE], recvBuf[BUFFER_SIZE];
+    int sentDataLen, recvDataLen;
+    recvInfo psend;
     string inputServAddress;
     cout << "Enter server address: \n";
     cin >> inputServAddress;
     cin.ignore();
 
-    if (argc >= 3){
-        strcpy(serverAddress,argv[1]);
+    if (argc >= 3) {
+        strcpy(serverAddress, argv[1]);
         serverPort = atoi(argv[2]);
-    }
-    else{
-        strcpy(serverAddress,inputServAddress.c_str());
+    } else {
+        strcpy(serverAddress, inputServAddress.c_str());
         serverPort = SERVER_PORT;
     }
 
     connSock = socket(AF_INET, SOCK_STREAM, 0);
-    if (connSock < 0){
+    if (connSock < 0) {
         cout << "Error when creating socket\n";
         exit(1);
     }
 
     bzero(&servAddr, sizeof(servAddr));
-    servAddr.sin_family      = AF_INET;
+    servAddr.sin_family = AF_INET;
     servAddr.sin_addr.s_addr = inet_addr(serverAddress);
-    servAddr.sin_port        = htons(serverPort);
-    if (connect(connSock, (struct sockaddr *)&servAddr, sizeof(servAddr)) < 0){
+    servAddr.sin_port = htons(serverPort);
+    if (connect(connSock, (struct sockaddr *) &servAddr, sizeof(servAddr)) < 0) {
         cout << "Error when connecting server!";
         exit(1);
     }
@@ -63,38 +69,43 @@ int main(int argc, char **argv) {
     /*
      * start loop send and recv msg to sever
      * */
-    while(1){
+    while (1) {
         bzero(recvBuf, BUFFER_SIZE);
-        cout << "Input a message to the echo server (QUIT for exit): ";
-        cin.getline(sentBuf, BUFFER_SIZE);
 
-        
-        if (strcmp(sentBuf,"QUIT") == 0){
-            write(connSock, sentBuf, strlen(sentBuf));
+        cout << "Input a message to the echo server (QUIT for exit): ";
+        //cin.getline(sentcharBuf, BUFFER_SIZE);
+        if (strcmp(sentcharBuf, "QUIT") == 0 || strcmp(sentcharBuf, "quit") == 0) {
+            send(connSock, sentcharBuf, strlen(sentcharBuf)+1,0);
             break;
         }
+        psend.cacheID = 10;
+        psend.taskType = 1;
+        strcpy(psend.key, VALUETEST.c_str());
+        cout << "Message Struct" << endl
+             << "cache ID: " << psend.cacheID << endl
+             << "tasktype: " << psend.taskType << endl
+             << "key: " << psend.key << endl;
+        strcpy(psend.value, VALUETEST.c_str());
+        sentDataLen = send(connSock, (char*)&psend, sizeof(psend)+1,0);
 
-        sentDataLen = write(connSock, sentBuf, strlen(sentBuf));
-
-        if (sentDataLen < 0){
+        if (sentDataLen < 0) {
             cout << "Error when sending data\n";
             break;
         }
         //
-        recvDataLen = read(connSock,recvBuf,sizeof(recvBuf));
-        if (recvDataLen < 0){
+        recvDataLen = recv(connSock, recvBuf, sizeof(recvBuf)+1,0);
+        if (recvDataLen < 0) {
             cout << "Error when receiving data\n";
             break;
-        }else{
-	    memset(&packet,0,sizeof(packet));
-		memcpy(&packet,recvBuf,sizeof(recvBuf));
+        } else {
+            memset(&revbuf, 0, sizeof(revbuf));
+            memcpy(&revbuf, recvBuf, sizeof(revbuf));
 
-
-            cout<<"Message received from server: "
-		<<"cacheID:   "<<packet.cacheID<<endl
-		<<"tasktype:  "<<packet.taskType<<endl
-		<<"Key:       "<<packet.key<<endl
-		<<"value:     "<<packet.value<<endl;
+            cout << "Message received from server: " << recvBuf << endl;
+            cout << "Message Struct" << endl
+                 << "cache ID: " << revbuf.cacheID << endl
+                 << "tasktype: " << revbuf.taskType << endl
+                 << "key: " << revbuf.key << endl;
         }
     }
     close(connSock);
